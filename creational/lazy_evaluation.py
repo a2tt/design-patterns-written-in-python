ca3cbaa4b96c0a1b61ebee085a5c4b4aa85687df
usera2tt <usera2tt@gmail.com>
2021-10-11 00:42:30 +0900

    creational - lazy evaluation

---
+++ creational/lazy_evaluation.py
@@ -0,0 +1,18 @@
+class Record:
+    def __init__(self, row_id):
+        self.row_id = row_id
+
+
+class Table:
+    lookup_table = {}
+
+    @classmethod
+    def get_record(cls, row_id: int) -> Record:
+        """ if row_id not in cls.lookup_table, instantiate Record object on the fly. """
+        if row_id not in cls.lookup_table.keys():
+            cls.lookup_table[row_id] = Record(row_id)  # There must be more args.
+        return cls.lookup_table[row_id]
+
+
+if __name__ == '__main__':
+    Table.get_record(1)
