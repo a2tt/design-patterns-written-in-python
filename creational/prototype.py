084c511ea109b0dd500ba9217a64f1688b7e14d7
usera2tt <usera2tt@gmail.com>
2021-10-26 15:02:57 +0900

    creational - prototype

---
+++ creational/prototype.py
@@ -0,0 +1,20 @@
+import copy
+
+
+class Prototype:
+    def __init__(self, name):
+        self.name = name
+        # there might be more expensive processes to initialize object
+
+    def clone(self, name=None):
+        _clone = copy.copy(self)  # or
+        if name:
+            _clone.name = name
+        return _clone
+
+
+if __name__ == '__main__':
+    p = Prototype(name='foo')
+    c = p.clone(name='bar')
+    print(id(p), p.name)  # 139728967565264 foo
+    print(id(c), c.name)  # 139728967562768 bar
