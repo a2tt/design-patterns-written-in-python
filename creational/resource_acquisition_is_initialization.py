667c64cdc827a70e87f71d41120a394e352492b1
usera2tt <usera2tt@gmail.com>
2021-10-28 23:55:56 +0900

    creational - RAII

---
+++ creational/resource_acquisition_is_initialization.py
@@ -0,0 +1,14 @@
+"""
+RAII(Resource-Acquisition-Is-Initialization) is not available on garbage collected languages like python.
+There should be scope-based deterministic destruction.
+
+In python, there is `with` statement. But it is not with RAII idiom.
+"""
+
+if __name__ == '__main__':
+    # using `with`, __enter__ and __exit__ methods are executed no matter what happens.
+    with open('test.log', 'wt') as fp:
+        pass
+
+    # but variable `fp` still there
+    print(fp)
