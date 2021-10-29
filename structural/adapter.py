ac6c341c48addc48be82992453995cb30daf2286
usera2tt <usera2tt@gmail.com>
2021-10-30 00:44:03 +0900

    change example code of `adapter`

--- structural/adapter.py
+++ structural/adapter.py
@@ -1,20 +1,46 @@
-class FaceBookOpenGraph:
+class AWSS3:
     @staticmethod
-    def print_sample():
-        print('<meta property="og:url" content="https://github.com/usera2tt">')
+    def get_object():
+        print('This is AWS S3 object')
 
 
-class TwitterOpenGraph:
+class AzureBlob:
     @staticmethod
-    def print_sample_card():
-        print('<meta name="twitter:card" content="summary">')
+    def get_blob():
+        print('This is Azure blob')
 
 
-class TwitterToFaceBookAdapter(TwitterOpenGraph):
-    def print_sample(self):
-        self.print_sample_card()
+class FileSystem:
+    @staticmethod
+    def get_file():
+        print('This is local file')
+
+
+class AWSS3ToFileSystemAdapter(AWSS3):
+    """ class adapter """
+
+    def get_file(self):
+        return self.get_object()
+
+
+class FileSystemAdapter:
+    """ object adapter """
+
+    def __init__(self, fs):
+        self.fs = fs
+
+    def get_file(self):
+        if type(self.fs) == AWSS3:
+            return self.fs.get_object()
+        elif type(self.fs) == AzureBlob:
+            return self.fs.get_blob()
+        elif type(self.fs) == FileSystem:
+            return self.fs.get_file()
 
 
 if __name__ == '__main__':
-    FaceBookOpenGraph.print_sample()
-    TwitterToFaceBookAdapter().print_sample()
+    AWSS3ToFileSystemAdapter().get_file()
+
+    FileSystemAdapter(AWSS3()).get_file()
+    FileSystemAdapter(AzureBlob()).get_file()
+    FileSystemAdapter(FileSystem()).get_file()
