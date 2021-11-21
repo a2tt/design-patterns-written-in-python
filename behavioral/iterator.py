1347df43c442f146f52bbfb1b25dbd98aa47cfac
usera2tt <usera2tt@gmail.com>
2021-11-21 23:30:34 +0900

    behavioral - iterator

---
+++ behavioral/iterator.py
@@ -0,0 +1,33 @@
+"""
+Python itself already implements the iterator pattern.
+"""
+
+
+class Counter:
+    def __init__(self, start: int, end: int):
+        self.num = start - 1
+        self.end = end
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.num < self.end:
+            self.num += 1
+            return self.num
+
+        raise StopIteration
+
+
+def counter(start: int, end: int):
+    for c in range(start, end + 1):
+        yield c
+
+
+if __name__ == '__main__':
+    for c in Counter(2, 5):
+        print(c)
+
+    for c in counter(6, 10):
+        print(c)
+      
