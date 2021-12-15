1cf5dab138a3746d206c53b1dfc2158a9aabfe77
usera2tt <usera2tt@gmail.com>
2021-12-15 23:39:10 +0900

    fix(creational): more detail implementation
    add difference between subclasses and add one more object creation in main func

--- creational/factory_method.py
+++ creational/factory_method.py
@@ -2,24 +2,32 @@ from abc import ABC, abstractmethod
 
 
 class Article:
+    """ Concrete class """
+
     def __init__(self, title: str, content: str):
         self.title = title
         self.content = content
 
+    def __repr__(self):
+        return f'{self.title} - {self.content[:50]}'
+
 
 class News(Article):
-    pass
+    def __init__(self, title: str, content: str, published_at: str):
+        super().__init__(title, content)
+        self.published_at = published_at
 
-
-class Post(Article):
-    pass
+    def __repr__(self):
+        return f'{self.published_at} | ' + super().__repr__()
 
 
 class Crawler(ABC):
+    """ class containing factory method """
+
     @classmethod
-    def crawl(cls):
-        # do crawling
-        page_content = ''  # crawled contents
+    def crawl(cls) -> Article:
+        # do crawling ...
+        page_content = ''  # crawled web content
         article = cls._parse_page(page_content)
         return article
 
@@ -33,20 +41,32 @@ class Crawler(ABC):
         raise NotImplementedError
 
 
-class NewsCrawler(Crawler):
-    @classmethod
-    def _parse_page(cls, page_content) -> Article:
-        # parse page_content
-        return News(title='', content='')
-
-
 class BlogCrawler(Crawler):
     @classmethod
     def _parse_page(cls, page_content) -> Article:
         # parse page_content
-        return Post(title='', content='')
+        title = 'Article title'
+        content = 'Article content'
+        return Article(title=title, content=content)
+
+
+class NewsCrawler(Crawler):
+    @classmethod
+    def _parse_page(cls, page_content) -> News:
+        # parse page_content
+        title = 'News title'
+        content = 'News content'
+        published_at = '2021-12-15'
+        return News(title=title, content=content, published_at=published_at)
+
+
+def main():
+    post = BlogCrawler.crawl()
+    news = NewsCrawler.crawl()
+
+    print(post)
+    print(news)
 
 
 if __name__ == '__main__':
-    news = NewsCrawler.crawl()
-    post = BlogCrawler.crawl()
+    main()
