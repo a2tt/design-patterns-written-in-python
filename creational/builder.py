169fd3992804d6c42e04c83eb03a1e606b81ae5f
usera2tt <usera2tt@gmail.com>
2021-12-17 21:50:34 +0900

    fix(creational): add little details to builder pattern

--- creational/builder.py
+++ creational/builder.py
@@ -9,6 +9,9 @@ class PushNotification:
         self.content = content
         self.extra = extra
 
+    def __repr__(self):
+        return f'To {self.user}\nURL: {self.url}\n{self.content}'
+
 
 class PushBuilder:
     def __init__(self):
@@ -31,14 +34,20 @@ class PushBuilder:
             self.extra.update(extra)
             self.extra = extra
 
-    def build(self):
+    def build(self) -> PushNotification:
         return PushNotification(self.user, self.url, self.content, self.extra)
 
 
-if __name__ == '__main__':
+def main():
     # There might be more complicate steps
     builder = PushBuilder()
     builder.set_user(456)
-    builder.set_content('python design pattern')
+    builder.set_url('https://github.com/usera2tt')
+    builder.set_content('design pattern in python')
 
     push = builder.build()
+    print(push)
+
+
+if __name__ == '__main__':
+    main()
