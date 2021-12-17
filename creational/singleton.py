932cf1df2b2e39d5d5d1b19c21dc6939877e429e
usera2tt <usera2tt@gmail.com>
2021-12-17 23:10:52 +0900

    chore(creational): add desc comments to singleton

--- creational/singleton.py
+++ creational/singleton.py
@@ -6,7 +6,8 @@ class Logger:
     _lock = threading.Lock()
 
     def __new__(cls):
-        with cls._lock:
+        """ Instantiate the singleton class if not created yet and return the object. """
+        with cls._lock:  # Prevent simultaneous instantiation in a multi threaded environment.
             if not cls._instance:
                 cls._instance = object.__new__(cls)
 
@@ -16,6 +17,10 @@ class Logger:
         print(*args)
 
 
-if __name__ == '__main__':
+def main():
     log = Logger()
     log.log('text')
+
+
+if __name__ == '__main__':
+    main()
