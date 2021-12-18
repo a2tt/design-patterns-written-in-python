54a5071b57dff94be6468881a3d788aea194fa77
usera2tt <usera2tt@gmail.com>
2021-12-18 20:14:45 +0900

    fix(creational): use `deepcopy` not `copy` and add annotation

--- creational/prototype.py
+++ creational/prototype.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import copy
 
 
@@ -6,15 +7,19 @@ class Prototype:
         self.name = name
         # there might be more expensive processes to initialize object
 
-    def clone(self, name=None):
-        _clone = copy.copy(self)  # or
+    def clone(self, name=None) -> Prototype:
+        _clone = copy.deepcopy(self)  # or
         if name:
             _clone.name = name
         return _clone
 
 
-if __name__ == '__main__':
+def main():
     p = Prototype(name='foo')
     c = p.clone(name='bar')
     print(id(p), p.name)  # 139728967565264 foo
     print(id(c), c.name)  # 139728967562768 bar
+
+
+if __name__ == '__main__':
+    main()
