c3f7e80cce7669485b57a44f79a74a72f1920593
usera2tt <usera2tt@gmail.com>
2021-12-18 19:12:00 +0900

    chore(creational): add desc comments to multiton

--- creational/multiton.py
+++ creational/multiton.py
@@ -2,6 +2,11 @@ class DataBase:
     REGISTRY = {}
 
     def __new__(cls, type_):
+        """
+        Instantiate the multiton class when `type_` has never been used to
+        created and return the object.
+        """
+        # Thread-safe operations required (ex. lock)
         if type_ not in cls.REGISTRY.keys():
             cls.REGISTRY[type_] = object.__new__(cls)
 
@@ -14,7 +19,11 @@ class DataBase:
         pass
 
 
-if __name__ == '__main__':
+def main():
     mysql = DataBase('mysql')
     postgresql = DataBase('postgresql')
     print(DataBase.REGISTRY)  # {'mysql': <...>, 'postgresql': <...>'}
+
+
+if __name__ == '__main__':
+    main()
