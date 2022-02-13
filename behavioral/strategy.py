ce51bda17a45ad716d00286129d040af6b1a0425
usera2tt <usera2tt@gmail.com>
2022-02-13 18:32:42 +0900

    feat(strategy): improve example code
    - I think creating `Searcher` object on every loop, doesnt look like strategy pattern but dependency injection.

--- behavioral/strategy.py
+++ behavioral/strategy.py
@@ -27,16 +27,15 @@ class BinarySearchStrategy(Strategy):
 
 
 class Searcher:
-    def __init__(self, items: List[int], strategy: Strategy):
-        self.items = items
+    def __init__(self, strategy: Strategy):
         self.strategy = strategy
 
     def set_strategy(self, strategy: Strategy):
         self.strategy = strategy
 
-    def find_idx(self, target: int) -> int:
-        print(f'Find {target} from list using {self.strategy.__class__.__name__}')
-        return self.strategy.find_idx(self.items, target)
+    def find_idx(self, items: List[int], target: int) -> int:
+        print(f'Find {target} from {items} using {self.strategy.__class__.__name__}')
+        return self.strategy.find_idx(items, target)
 
 
 def main():
@@ -45,11 +44,14 @@ def main():
         [-3, -1, 1, 2, 4, 7, 9, 10]
     ]
 
+    sequential_strategy = SequentialStrategy()
+    binary_strategy = BinarySearchStrategy()
+    searcher = Searcher(sequential_strategy)
+
     for items in tests:
-        is_sorted = sorted(items) == items
-        strategy = BinarySearchStrategy if is_sorted else SequentialStrategy
-        searcher = Searcher(items, strategy())
-        print(searcher.find_idx(2))
+        strategy = binary_strategy if sorted(items) == items else sequential_strategy
+        searcher.set_strategy(strategy)
+        print(searcher.find_idx(items, 2))
 
 
 if __name__ == '__main__':
